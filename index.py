cc3481662621450a5683d24e60a5368cf0c4d7fc
Daniel Golia Conti <contidan@msu.edu>
2026-08-25 18:51:07 -0400

    dotenv fix

--- index.py
+++ index.py
@@ -5,15 +5,12 @@ from dotenv import load_dotenv
 from twilio.twiml.voice_response import Record, VoiceResponse
 import requests
 
-# Load the environment variables
 load_dotenv()
 
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
-account_sid = os.environ["TWILIO_ACCOUNT_SID"]
-auth_token = os.environ["TWILIO_AUTH_TOKEN"]
-client = Client(account_sid, auth_token)
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
 
+client = Client(account_sid, auth_token)
 
 
 def callTwilio():
